389daa8810fa645e01db5465d286f31b069b0456
devpatel-21 <166136833+devpatel-21@users.noreply.github.com>
2024-06-24 12:46:19 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music={
+    "permish" : "https://www.youtube.com/watch?v=muds1gFUTN8",
+    "chalisa" : "https://www.youtube.com/watch?v=AETFvQonfV8",
+    "vishvambhari" : "https://www.youtube.com/watch?v=MYkEYAoqFTs",
+    "aadhyasakti" : "https://www.youtube.com/watch?v=SU8aghkgeCQ",
+}
