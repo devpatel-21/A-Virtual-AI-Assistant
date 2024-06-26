c669aa49511a43e064389775af028ebbcb1012b5
devpatel-21 <166136833+devpatel-21@users.noreply.github.com>
2024-06-26 10:38:36 +0530

    Update musicLibrary.py

--- musicLibrary.py
+++ musicLibrary.py
@@ -1,6 +1,6 @@
 music={
     "permish" : "https://www.youtube.com/watch?v=muds1gFUTN8",
-    "chalisa" : "https://www.youtube.com/watch?v=AETFvQonfV8",
-    "vishvambhari" : "https://www.youtube.com/watch?v=MYkEYAoqFTs",
-    "aadhyasakti" : "https://www.youtube.com/watch?v=SU8aghkgeCQ",
+    "moonlight" : "https://www.youtube.com/watch?v=TUawyJn7dws",
+    "kabira" : "https://www.youtube.com/watch?v=jHNNMj5bNQw",
+    "intouch" : "https://www.youtube.com/watch?v=QJKrGkRCS5g&list=PLRBp0Fe2GpgnIh0AiYKh7o7HnYAej-5ph&index=20",
 }
